6cd273b1b609a4a6ceec8f22a750d87506b79511
MFJcode <mfjcode@gmail.com>
2022-06-22 08:45:47 +0200

    function mult

---
+++ index.js
@@ -0,0 +1,12 @@
+function mult(value) {
+  let promptValue = value;
+
+  let promptVL = document.querySelector(".promptVL");
+  let meterVL = document.querySelector(".meters");
+  let feetsVL = document.querySelector(".feets");
+  let gallonVL = document.querySelector(".gallon");
+  let litersVL = document.querySelector(".liters");
+  let kilosVL = document.querySelector(".kilos");
+  let poundsVL = document.querySelector(".pounds");
+  
+}
