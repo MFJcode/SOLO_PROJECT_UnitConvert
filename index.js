101caccacf492ff6f3971f56df1f0852998cbbcc
MFJcode <mfjcode@gmail.com>
2022-06-30 11:34:28 +0200

    conversion

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ function mult(value) {
   let gallonVL = document.querySelector(".gallon");
   let litersVL = document.querySelector(".liters");
   let kilosVL = document.querySelector(".kilos");
-  let poundsVL = document.querySelector(".pounds");
-  
+  let poundsVL = document.querySelector(".pounds");  
 }
+
+// Feet to Meters
+  meter = promptValue / 3.2808;
+  // Meters to Feet
+  feet = promptValue * 3.2808;
+
+  // Liters to gallon
+  gallon = promptValue / 3.785;
+
+  // gallon to Liters
+  liter = promptValue * 3.785;
+
+  // Kilograms to Pounds
+  kilo = promptValue * 2.2046;
+
+  // Pound to kilo
+  pound = promptValue / 2.2046;
